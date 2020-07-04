915e7a9824bb141b6b1c4a274ee6924187e88ecd
Kanta Yamaoka (山岡幹太) <49053649+kantasv@users.noreply.github.com>
2020-07-04 18:03:48 +0900

    Update crowdworks-optimizer.js

--- crowdworks-optimizer.js
+++ crowdworks-optimizer.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Kanta Yamaoka
 // @match        https://crowdworks.jp/*
 // @grant        none
 // ==/UserScript==
